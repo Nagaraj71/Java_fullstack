dad954f3c1179f21e2e6b0bbfe23e5159fe5e851
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-03-11 23:14:31 +0530

    Ok

--- javascript/Reactjs/two.js
+++ javascript/Reactjs/two.js
@@ -1,7 +1,3 @@
-let emp={ID:101, Name:"Nagaraj",Email:"goudanagaraj789@gmail.com"}
-emp.ID=103;
-emp.Name="Nagaraj Gouda";
-emp.Email="goudanagaraj789@gmail.com";
-emp.Salary=45000;
-
-console.log(emp);
+function add(a,b){
+    console.log("addition")
+}add(5,10)
