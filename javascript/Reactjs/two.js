66dd0407c8801e38a782798d1fd265433890bed4
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-03-11 23:09:19 +0530

    Ok

--- javascript/Reactjs/two.js
+++ javascript/Reactjs/two.js
@@ -2,4 +2,5 @@ let emp={ID:101, Name:"Nagaraj",Email:"goudanagaraj789@gmail.com"}
 emp.ID=103;
 emp.Name="Nagaraj Gouda";
 emp.Email="goudanagaraj789@gmail.com";
+emp.Salary=45000;
 console.log(emp)
