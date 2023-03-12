80a9a9ee2d7ca34a7274db447a6731633ebcfe3d
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-03-13 01:02:56 +0530

    Ok

--- javascript/Reactjs/two.js
+++ javascript/Reactjs/two.js
@@ -1,8 +1,8 @@
 let emp={Id:101, Name:"Rahul Gandhi",Email:"goudanagaraj789@gmail.com"}
 
 if(Object.keys(emp)){
-    console.log("Empty Object")
+    console.log("Empty Object");
 }else
-console.log("Not an Empty Object")
+console.log("Not an Empty Object");
 
 
