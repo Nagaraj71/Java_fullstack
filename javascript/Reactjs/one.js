8635adb3eff202eaf864611c6a0cbbe36aa6b69b
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-03-15 22:22:40 +0530

    ok

--- javascript/Reactjs/one.js
+++ javascript/Reactjs/one.js
@@ -4,18 +4,3 @@ if(salary>40000){
 }
 else{
     console.log("Stay in rented house")}
-
-
-    // checking
-/* 
-    if(salary>40000){
-        console.log("construct the home")
-    }
-    else{
-        console.log("Stay in rented house")}
-    
-    
-        // checking */
-    
-
-
