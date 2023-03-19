05f231b515816ae8140476948d6b1bf2b58da863
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-03-19 21:23:32 +0530

    Ok

--- javascript/Reactjs/SpreadOp/ES6Feature/four.js
+++ javascript/Reactjs/SpreadOp/ES6Feature/four.js
@@ -1,5 +1,9 @@
-class users{
+class customer{
+    min_Bal=500
+    Name="Rahul Gandhi"
+}
 
-}console.log(new users)
-console.log(new users)
-console.log(new users)
+let C1= new customer();
+let c2= new customer();
+console.log(c2.min_Bal)
+console.log(c2.Name)
