d151ba24ca045116b1be6fc4491d1eb36a320803
Nagaraj71 <115916259+Nagaraj71@users.noreply.github.com>
2023-06-07 23:30:53 +0530

    Ok

--- javascript/Reactjs/SpreadOp/ES6Feature/four.js
+++ javascript/Reactjs/SpreadOp/ES6Feature/four.js
@@ -1,7 +1,2 @@
 let salary=45000;
-
-if(salary>40000){
-    console.log("Eligible for marriage")
-}else{
-    console.log("Not Eligible")
-}
+salary>40000? console.log("Eligible for Marriage"): console.log("Not Eligible")
